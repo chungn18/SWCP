026ccdb17995402b83553d70a3e7d550f13da69e
chungn18 <vanchung07d1@gmail.com>
2022-10-10 00:40:44 +0700

    Program and table in excel run OK but need defect Trace() function

--- DynamicPro/editDistance/main.cpp
+++ DynamicPro/editDistance/main.cpp
@@ -1,55 +1,76 @@
 #include <iostream>
 
 using namespace std;
-const int MAX = 1000;
-int Wmax;
+const int MAX = 2000;
 int n = 0;
-int w[MAX];
-int v[MAX];
-int F[MAX][MAX]; //mang luu gia tri Vmax trong cac goi {0,1,2,...,n-1} co khoi luong w(i) voi i{0,1,2,..,n-1}
+string X[10];
+string Y[10];
+int Nteca;
+int F[MAX][MAX];
 
 void inputData(){
-    cout << "Input data follow syntax" << endl;
-    cout << "Line 1: Wmax n" << endl;
-    cout << "Line i: w[i] v[i]" << endl;
-    cin >> Wmax;
-    cin >> n;
-    for (int i = 1; i <= n; i++){
-        cin >> w[i];
-        cin >> v[i];
+    cout << "Input number of test case and 2 strings X, Y as syntax: " << endl;
+    cout << "Number of test case" << endl;
+    cout << "Xi Yi (with i from 1 to N, i is int)" << endl; 
+    cin >> Nteca;
+    for (int i=0; i< Nteca; i++){
+        cin >> X[i];
+        X[i].insert(0," "); //make string begin from at 1
+        cin >> Y[i];
+        Y[i].insert(0," "); //make string begin from at 1
     }
 }
-void printRes()
+void Trace(int lenX,int lenY)
 {
-    int i=n;
-    int j=Wmax;
-    cout << "Max value: " << F[n][Wmax] << endl;
-    cout << "w v" <<endl;
-    while (i >0){
-        if (F[i][j] != F[i-1][j]){
-            cout << w[i] <<" " << v[i] <<endl;
-            j = j- w[i];
+    int i = lenX;
+    int j = lenY;
+    for (int i=lenX; i>=0; i--){
+        for (int j=lenY; j>=0; j--){
+            if (F[i][j]==F[i-1][j-1]){
+                i--;j--;
+            }
+            else if (F[i][j]==(F[i][j-1]+1))
+            {
+                cout << "Insert at " << i << Y[i] << endl;
+            }
+            else if (F[i][j]==(F[i-1][j]+1)){
+                cout << "Delete at " << i << Y[i] << endl;
+            }
+            else {
+                cout << "Replace at " << i << Y[i] << endl;
+            }
+
         }
-        --i;
     }
 
+
 }
-void resolve(){
+int resolve(string x, string y){
     memset(F,0,MAX*MAX);
-    for (int i =1; i <= n; i++){
-        for (int j=1; j<= Wmax; j++ )
+    for (int i=0; i<=x.length(); i++)
+    F[i][0]=i;
+    for (int j=0; j<=y.length(); j++)
+    F[0][j]=j;
+    
+    for (int i =1; i <= x.length(); i++){
+        
+        for (int j=1; j<= y.length(); j++ )
         {
-            F[i][j] = F[i-1][j]; // gan cho F[i][j] gia tri truoc do de kiem tra truoc khi cap nhap
-            if((F[i][j] < F[i-1][j-w[i]] + v[i]) && j>=w[i]){ //neu gia tri truoc nho hon gia tri dc chon va j >=w[i] thi cap nhat nhat gia tri moi
-                F[i][j] = F[i-1][j-w[i]] + v[i];
+            F[i][j] = F[i-1][j-1];
+            if(x[i]!=y[j]){
+                F[i][j] = min(min(F[i][j-1],F[i-1][j]),F[i-1][j-1])+1;
             }
         }
     }
+    cout << "Number of operaters: " << F[x.length()][y.length()] << endl;
+    return F[x.length()][y.length()];
 }
 
 int main(){
     inputData();
-    resolve();
-    printRes();
+    for (int i=0; i<Nteca; i++){
+    resolve(X[i],Y[i]);
+    Trace(X[i].length(),Y[i].length());
+    }
 
 }
