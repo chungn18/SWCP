107b8c56a3f502e25fac4ae31702f4c153569410
chungn18 <vanchung07d1@gmail.com>
2022-10-11 01:05:22 +0700

    fucntion resolve() run ok, but Trace() is NOK

--- DynamicPro/editDistance/main.cpp
+++ DynamicPro/editDistance/main.cpp
@@ -15,9 +15,9 @@ void inputData(){
     cin >> Nteca;
     for (int i=0; i< Nteca; i++){
         cin >> X[i];
-        X[i].insert(0," "); //make string begin from at 1
+        //X[i].insert(0," "); //make string begin from at 1
         cin >> Y[i];
-        Y[i].insert(0," "); //make string begin from at 1
+        //Y[i].insert(0," "); //make string begin from at 1
     }
 }
 void Trace(string x, string y)
@@ -25,23 +25,24 @@ void Trace(string x, string y)
     int i = x.length()-1;
     int j = y.length()-1;
     int count =1;
-    while (i>0 || j>0){
+    while (i>=0 || j>=0){
         if (F[i][j]==F[i-1][j-1]){
             i--;j--;
         }
-        else if ((j>0)&&F[i][j]==(F[i][j-1]+1))
+        else if ((j>=0)&&F[i][j]==(F[i][j-1]+1))
         {
             cout << count << ".Insert at " << i << " char " << y[j] << endl;
-            i--;
+            j--;
             count++;
         }
-        else if ((i>0)&&(j>0)&&F[i][j]==(F[i-1][j]+1)){
-            cout << count << ".Replace at " << i << " char " << x[i] << endl;
+        else if ((i>=0)&&(j>0)&&F[i][j]==(F[i-1][j]+1)){
+            cout << count << ".Replace at " << i << " char " << x[i] << " by char " << y[j] << endl;
             i--;j--;
             count++;
         }
         else {
             cout << count << ".Delete at " << i << " char " << x[i] << endl;
+            i--;
             count++;
         }
     }
@@ -50,23 +51,27 @@ void Trace(string x, string y)
 }
 int resolve(string x, string y){
     memset(F,0,MAX*MAX);
-    for (int i=0; i<=x.length(); i++)
+    for (int i=0; i<x.length(); ++i)
     F[i][0]=i;
-    for (int j=0; j<=y.length(); j++)
+    for (int j=0; j<y.length(); ++j)
     F[0][j]=j;
     
-    for (int i =1; i <= x.length(); i++){
+    for (int i =1; i < x.length(); ++i){
         
-        for (int j=1; j<= y.length(); j++ )
+        for (int j=1; j< y.length(); ++j)
         {
-            F[i][j] = F[i-1][j-1];
-            if(x[i]!=y[j]){
+            
+            if(x[i-1]!=y[j-1]){
                 F[i][j] = min(min(F[i][j-1],F[i-1][j]),F[i-1][j-1])+1;
+            }
+            else
+            {
+                F[i][j] = F[i-1][j-1];
             }
         }
     }
-    cout << "Number of operaters: " << F[x.length()][y.length()] << endl;
-    return F[x.length()][y.length()];
+    cout << "Number of operaters: " << F[x.length()-1][y.length()-1] << endl;
+    return F[x.length()-1][y.length()-1];
 }
 
 int main(){
