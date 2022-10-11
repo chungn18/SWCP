220493b664ffeaa00175c268f40c3174c59f4664
chungn18 <vanchung07d1@gmail.com>
2022-10-11 22:50:23 +0700

    Try test case 2 to trace operators but fails -> continue study

--- DynamicPro/editDistance/main.cpp
+++ DynamicPro/editDistance/main.cpp
@@ -22,56 +22,56 @@ void inputData(){
 }
 void Trace(string x, string y)
 {
-    int i = x.length()-1;
-    int j = y.length()-1;
+    int i = y.length();
+    int j = x.length();
     int count =1;
-    while (i>=0 || j>=0){
+    while (i>0 || j>0){
         if (F[i][j]==F[i-1][j-1]){
             i--;j--;
         }
-        else if ((j>=0)&&F[i][j]==(F[i][j-1]+1))
+        else if ((i>0)&&F[i][j]==(F[i-1][j]+1))
         {
-            cout << count << ".Insert at " << i << " char " << y[j] << endl;
-            j--;
+            cout << count << ".Insert(" << j << ","<< y[i-1] << ")" << endl;
+            i--;
             count++;
         }
-        else if ((i>=0)&&(j>0)&&F[i][j]==(F[i-1][j]+1)){
-            cout << count << ".Replace at " << i << " char " << x[i] << " by char " << y[j] << endl;
+        else if ((i>0)&&(j>0)&&F[i][j]==(F[i-1][j-1]+1)){
+            cout << count << ".Replace(" << j << "," << x[j-1] << "," << y[i-1] << ")"<< endl;
             i--;j--;
             count++;
         }
         else {
-            cout << count << ".Delete at " << i << " char " << x[i] << endl;
-            i--;
+            cout << count << ".Delete(" << j << "," << x[j-1] << ")" << endl;
+            j--;
             count++;
         }
     }
 
 
 }
-int resolve(string x, string y){
+void resolve(string x, string y){
     memset(F,0,MAX*MAX);
-    for (int i=0; i<x.length(); ++i)
+    for (int i=0; i<=y.length(); ++i)
     F[i][0]=i;
-    for (int j=0; j<y.length(); ++j)
+    for (int j=0; j<=x.length(); ++j)
     F[0][j]=j;
     
-    for (int i =1; i < x.length(); ++i){
+    for (int i =1; i <= y.length(); ++i){
         
-        for (int j=1; j< y.length(); ++j)
+        for (int j=1; j<= x.length(); ++j)
         {
             
-            if(x[i-1]!=y[j-1]){
-                F[i][j] = min(min(F[i][j-1],F[i-1][j]),F[i-1][j-1])+1;
+            if(x[j-1]==y[i-1]){
+                F[i][j] = F[i-1][j-1];
             }
             else
             {
-                F[i][j] = F[i-1][j-1];
+                
+                F[i][j] = min(min(F[i-1][j],F[i][j-1]),F[i-1][j-1])+1;
             }
         }
     }
-    cout << "Number of operaters: " << F[x.length()-1][y.length()-1] << endl;
-    return F[x.length()-1][y.length()-1];
+    cout << "Number of operaters: " << F[y.length()][x.length()] << endl;
 }
 
 int main(){
