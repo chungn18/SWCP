ca19d89260301e8b1be1cb6b08db3b2745b09db2
chungn18 <vanchung07d1@gmail.com>
2023-02-14 22:11:32 +0700

    solved problem 2

---
+++ Grind_75/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+string S;//first integer
+
+bool isValid(string s) {
+    stack<char> st;
+    char c;
+    for (std::string::iterator it = s.begin(); it != s.end(); ++it){
+        if (*it ==  '(' || *it == '[' || *it == '{'){
+            st.push(*it);
+            cout << "Push new character: " << st.top() << endl;
+        }
+
+        else if ((*it ==  ')' || *it == ']' || *it == '}') && !st.empty()) {
+            c = st.top();
+            st.pop();
+            cout << "Close character: " << *it << endl;
+            if (*it == ')' && c != '(') return false;
+            if (*it == ']' && c != '[') return false;
+            if (*it == '}' && c != '{') return false;
+        }
+
+        else return false;
+    }
+
+    if (st.empty()) return true;
+    else return false;
+}
+
+int main(){
+
+	string str;
+	cin >> str;
+	cout << "Output: " << isValid(str) << endl;
+	return 0;
+}
